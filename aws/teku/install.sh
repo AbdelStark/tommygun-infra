719aeb8ee7a9bbcf65549020568d4906f8070a6d
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-11-27 18:26:54 +0100

    teku

---
+++ aws/teku/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+entryDir=$(pwd)
+rootDir=${1:-/home/ec2-user}
+workDir="$rootDir/tmp"
+configDir=/home/ec2-user/config
+dataDir=/home/ec2-user/data
+logDir=/var/log/teku
+
+# install requirements
+yum install -y git java
+git clone https://github.com/ConsenSys/teku
+cd teku
+./gradlew install
+
+# configure besu
+mkdir -p $workDir $configDir $dataDir $logDir
+cd $workDir
+
+tekuBeaconServiceFileURL=https://raw.githubusercontent.com/abdelhamidbakhta/tommygun-infra/main/aws/teku/teku-beacon.service
+tekuValidatorServiceFileURL=https://raw.githubusercontent.com/abdelhamidbakhta/tommygun-infra/main/aws/teku/teku-validator.service
+
+wget $tekuBeaconServiceFileURL $tekuValidatorServiceFileURL  > /dev/null 2>&1
+cp $tekuBeaconServiceFileURL /etc/systemd/system/teku-beacon.service
+cp $tekuValidatorServiceFileURL /etc/systemd/system/teku-validator.service
+
+rm -Rf $workDir
+cd $entryDir
+
+sudo systemctl daemon-reload
+sudo chown -R ec2-user:ec2-user $configDir $dataDir $logDir
