b46ddab93d91fee349a075f79d9d386a2ac94651
Abdelhamid Bakhta <45264458+abdelhamidbakhta@users.noreply.github.com>
2020-11-30 09:44:43 +0100

    Update install.sh

--- aws/teku/install.sh
+++ aws/teku/install.sh
@@ -18,13 +18,19 @@ cd $workDir
 
 tekuBeaconServiceFileURL=https://raw.githubusercontent.com/abdelhamidbakhta/tommygun-infra/main/aws/teku/teku-beacon.service
 tekuValidatorServiceFileURL=https://raw.githubusercontent.com/abdelhamidbakhta/tommygun-infra/main/aws/teku/teku-validator.service
+tekuBeaconScriptURL=https://raw.githubusercontent.com/abdelhamidbakhta/tommygun-infra/main/aws/teku/teku-beacon.sh
+tekuValidatorScriptURL=https://raw.githubusercontent.com/abdelhamidbakhta/tommygun-infra/main/aws/teku/teku-validator.sh
 
 wget $tekuBeaconServiceFileURL $tekuValidatorServiceFileURL  > /dev/null 2>&1
 cp teku-beacon.service /etc/systemd/system/teku-beacon.service
 cp teku-validator.service /etc/systemd/system/teku-validator.service
+wget $tekuBeaconScriptURL $tekuValidatorScriptURL > /dev/null 2>&1
+cp teku-beacon.sh $rootDir
+cp teku-validator.sh $rootDir
 
+chmod +x $rootDir/*.sh
 rm -Rf $workDir
 cd $entryDir
 
-sudo systemctl daemon-reload
-sudo chown -R ec2-user:ec2-user $configDir $dataDir $logDir
+systemctl daemon-reload
+chown -R ec2-user:ec2-user $configDir $dataDir $logDir
