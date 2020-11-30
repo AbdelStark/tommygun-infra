28e551c52734b4e3a7a0625dc5f592f9699b89e6
Abdelhamid Bakhta <45264458+abdelhamidbakhta@users.noreply.github.com>
2020-11-30 09:39:42 +0100

    Create teku-beacon.sh

---
+++ aws/teku/teku-beacon.sh
@@ -0,0 +1,7 @@
+/home/ec2-user/teku/build/install/teku/bin/teku \
+--network=pyrmont \
+--data-path=/home/ec2-user/data/beacon \
+--eth1-endpoint=https://goerli.infura.io/v3/7afd3452135040c3ab7da33ba6d57069 \
+--eth1-deposit-contract-address=0x8c5fecdC472E27Bc447696F431E425D02dd46a8c \
+--rest-api-enabled --rest-api-host-allowlist="*" \
+--logging=DEBUG
