2a1abde398ab727046e284b857e92278f657c7d1
Abdelhamid Bakhta <45264458+abdelhamidbakhta@users.noreply.github.com>
2020-11-30 09:41:30 +0100

    Create teku-validator.sh

---
+++ aws/teku/teku-validator.sh
@@ -0,0 +1,2 @@
+/home/ec2-user/teku/build/install/teku/bin/teku validator-client --beacon-node-api-endpoint=http://127.0.0.1:5051 \
+--validator-keys="/home/ec2-user/config/keystore.json:/home/ec2-user/config/keystore.txt"
