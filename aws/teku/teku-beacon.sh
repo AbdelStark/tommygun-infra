aad81040c77ce544186d60ba978621dd666b4cd5
Abdelhamid Bakhta <45264458+abdelhamidbakhta@users.noreply.github.com>
2020-12-01 09:58:44 +0100

    Update teku-beacon.sh

--- aws/teku/teku-beacon.sh
+++ aws/teku/teku-beacon.sh
@@ -4,4 +4,6 @@
 --eth1-endpoint=https://goerli.infura.io/v3/7afd3452135040c3ab7da33ba6d57069 \
 --eth1-deposit-contract-address=0x8c5fecdC472E27Bc447696F431E425D02dd46a8c \
 --rest-api-enabled --rest-api-host-allowlist="*" \
---logging=DEBUG
+--rest-api-cors-origins="*" \
+--metrics-enabled --metrics-host-allowlist="*" --metrics-interface="0.0.0.0" \
+--logging=INFO
